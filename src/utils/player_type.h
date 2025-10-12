429dc099aed3ca7d37139d102f79ce8e889618c9
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-10-12 16:42:20 -0300

    adicionar função para converter tipos de jogador em string

--- src/utils/player_type.h
+++ src/utils/player_type.h
@@ -7,4 +7,12 @@ enum PlayerType {
     COUNT // Deve ser o último elemento, usado para contar o número de tipos de jogadores
 };
 
+inline std::string playerTypeToString(PlayerType type) {
+    switch (type) {
+        case BLUE: return "BLUE";
+        case PINK: return "PINK";
+        default: return "UNKNOWN";
+    }
+}
+
 #endif //PLAYER_TYPE_H
